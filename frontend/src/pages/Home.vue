464f92925963664366379eb041daf3dd82c7c63c
“rehanrehman389” <rehanrehman389@gmail.com>
2024-11-02 11:43:17 +0530

    setup project

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <MainLayout>
+    <div class="bg-green-500">HOME.VUE</div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+</script>

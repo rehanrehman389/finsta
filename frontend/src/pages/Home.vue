e29bb369cf496ff992bd46c1adceba39aa589b73
“rehanrehman389” <rehanrehman389@gmail.com>
2024-11-03 16:47:03 +0530

    Home Page

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,9 +1,101 @@
+<script setup>
+  import { ref, onMounted, toRefs } from 'vue'
+  import { Head, Link, router } from '@inertiajs/vue3'
+  import MainLayout from '@/Layouts/MainLayout.vue';
+
+  import 'vue3-carousel/dist/carousel.css'
+  import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
+  import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+  import LikesSection from '@/Components/LikesSection.vue';
+
+  let width = ref(window.innerWidth)
+  let currentSlide = ref(0)
+  let currentPost = ref(null)
+  let openOverlay = ref(false)
+
+  const props = defineProps({ posts: Object, allUsers: Object})
+  const { posts, allUsers } = toRefs(props)
+
+  onMounted(() => {
+    window.addEventListener('resize', () => {
+      width.value = window.innerWidth
+    })
+  })
+</script> 
+
 <template>
   <MainLayout>
-    <div class="bg-red-500">HOME.VUE</div>
+    <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0">
+      <Carousel
+        v-model="currentSlide"
+        class="max-w-[700px] mx-auto"
+        :items-to-show="width >= 768 ? 8 : 6"
+        :items-to-scroll="4"
+        :wrap-around="true"
+        :transition="500"
+        snapAlign="start"
+      >
+      <slide v-for="slide in 10" :key="slide">
+        <Link href="/" class="relative mx-auto text-center mt-4 px-2 cursor-pointer">
+          <div class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500">
+            <div class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white" />
+          </div>
+          <img src="https://picsum.photos/id/54/300/320" class="rounded-full w-[56px] -mt-[1px]">
+          <div class="text-xs mt-2 w-[60px] truncate text-ellipsis overflow-hidden">Name Here</div>
+        </Link>
+      </slide>
+
+      <template #addons>
+        <Navigation />
+      </template>
+      </Carousel>
+
+      <div id="Posts" class="px-4 max-w-[600px] mx-auto mt-10">
+        <div class="flex items-center justify-between py-2">
+          <div class="flex items-center">
+            <Link href="/" class="flex items-center">
+              <img src="https://picsum.photos/id/54/300/320" class="rounded-full w-[38px] h-[38px]">
+              <div class="ml-4 font-extrabold text-[15px]">Name Here</div>
+            </Link>
+            <div class="flex items-center text-[15px] text-gray-500">
+              <span class="-mt-5 ml-2 mr-[5px] text-[35px]">.</span>
+              <div>Date Here</div>
+            </div>
+          </div>
+
+          <DotsHorizontal class="cursor-pointer" :size="27" />
+        </div>
+
+        <div class="bg-black rounded-lg w-full min-h-[400px] flex items-center">
+          <img src="https://picsum.photos/id/54/300/320" class="max-auto w-full">
+        </div>
+
+        <LikesSection />
+        <div class="text-black font-extrabold py-1">3 likes</div>
+        <div>
+          <span class="text-black font-extrabold">Name Here</span>
+            this is some text here
+        </div>
+        <button class="text-gray-500 font-extrabold py-1">
+          View all 4 comments
+        </button>
+      </div>
+
+      <div class="pb-20"></div>
+    </div>
+
   </MainLayout>
 </template>
 
-<script setup>
-import MainLayout from '@/Layouts/MainLayout.vue';
-</script>
+<style>
+.carousel_prev,
+.carousel_next,
+.carousel_prev:hover,
+.carousel_next:hover {
+    color: rgb(49, 49, 49);
+    background-color: rgb(255, 255, 255);
+    border-radius: 100%;
+    margin: 0 20px;
+}
+</style>
